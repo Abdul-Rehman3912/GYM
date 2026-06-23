6b58c20446a150f1f48e9d2c62effed641fd4663
Abdul Rehman <abdulrahmankhan.8x@gmail.com>
2026-06-23 09:39:45 +0500

    Connected axios instance to new live Render backend

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
     // baseURL: "http://localhost:5000/api",
-    baseURL: "https://e-commerce-backend-sysmatixx.vercel.app/api",
+    baseURL: "https://e-commerce-backend-sysmatixx-1.onrender.com/api",
     withCredentials: true,
 });
