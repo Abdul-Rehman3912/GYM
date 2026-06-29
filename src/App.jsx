c07f8ad920bdff5f2c1146d5ba5cf7bb5bb9f544
Abdul Rehman <abdulrahmankhan.8x@gmail.com>
2026-06-29 16:27:41 +0500

    Format Code

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,27 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero.jsx";
 import Equipments from "./components/Equipments/Equipments";
-import Banner from "./components/Banner/Banner.jsx"
-import Banner2 from "./components/Banner/Banner2.jsx"
+import Banner from "./components/Banner/Banner.jsx";
+import Banner2 from "./components/Banner/Banner2.jsx";
 import BgImage from "./assets/bg.png";
 import TabComp from "./components/Tab/TabComp.jsx";
-import Footer from "./components/Footer/Footer.jsx"
+import Footer from "./components/Footer/Footer.jsx";
 
-
-import Img1 from "../src/assets/2.png"
-import Img2 from "../src/assets/3.png"
+import Img1 from "../src/assets/2.jpg";
+import Img2 from "../src/assets/3.png";
 
 const BannerData = {
   image: Img1,
   title: "The Importants To Take Care Of Yourself",
   subtitle:
-    "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus.",
+    "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus. The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus. The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus. The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus. The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus.",
   link: "#",
 };
 const Banner2Data = {
   image: Img2,
   title: "The Importants To Take Care Of Yourself",
   subtitle:
-    "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus.",
+    "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus. ",
   link: "#",
 };
 
@@ -42,11 +41,11 @@ const App = () => {
         <Hero />
       </div>
       <Equipments />
-      <Banner {...BannerData}/>
+      <Banner {...BannerData} />
       <TabComp />
-      <Banner {...Banner2Data}/>
-      <Banner2/>
-      <Footer/>
+      <Banner {...Banner2Data} />
+      <Banner2 />
+      <Footer />
     </div>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Play } from "lucide-react";
 import HeroImg from "../../assets/dumbell.png";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import { SlideRight } from "../../utility/animation";
 
 function Hero() {
@@ -11,27 +11,30 @@ function Hero() {
         <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px] relative">
           <div className="flex flex-col justify-center py-14 md:py-0 font-playfair">
             <div className="text-center md:text-left space-y-6">
-              <motion.h1 
-              variants={SlideRight(0.6)}
-              initial="hidden"
-              animate="visible"
-              className="text-5xl md:text-[50px] lg:text-6xl font-bold leading-relaxed xl:leading-normal">
+              <motion.h1
+                variants={SlideRight(0.6)}
+                initial="hidden"
+                animate="visible"
+                className="text-5xl md:text-[50px] lg:text-6xl font-bold leading-relaxed xl:leading-normal"
+              >
                 Gym gives you a <br /> Perfect{" "}
                 <span className="text-primary block md:inline">Health</span>
               </motion.h1>
-              <motion.p 
+              <motion.p
                 variants={SlideRight(1.2)}
-              initial="hidden"
-              animate="visible"
-              className="text-gray-600 xl:max-w-[500px]">
+                initial="hidden"
+                animate="visible"
+                className="text-gray-600 xl:max-w-[500px]"
+              >
                 It is a long established fact that a reader will be by readabel
                 Content of a page when are the best product.
               </motion.p>
-              <motion.div 
+              <motion.div
                 variants={SlideRight(1.5)}
-              initial="hidden"
-              animate="visible"
-              className="flex justify-center gap-8 md:justify-start">
+                initial="hidden"
+                animate="visible"
+                className="flex justify-center gap-8 md:justify-start"
+              >
                 <button className="mt-10 primary-btn bg-primary w-[120px] h-10 p-2 flex item-center justify-center gap-2 rounded-xl text-white">
                   Order Now
                 </button>
